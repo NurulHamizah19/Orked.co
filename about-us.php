9fe3653285d734e6a8b42a32512d7ba2372b4f4c
tnfirdaustmka <138692339+tnfirdaustmka@users.noreply.github.com>
2023-07-09 22:34:04 +0800

    adding color

--- about-us.php
+++ about-us.php
@@ -32,7 +32,9 @@ include_once 'template/header.php';
 					<div class="p-3 bg-light">
 						<div class="container">
 							<h4>Our Story</h4>
-							<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+							<p>Orked.Co is a local brand that sells formal apparel for women and children. It is actually a widely known local brand for the latest design of kurung and it has been operating since 2014. The main boutique is in Bukit Mertajam, Penang but we are also operating as an online business which is through social media such as Instagram and Whatsapp.
+								Owned by a spouse and with the help of 3 workers.
+							</p>
 							<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.</p>
 							<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.</p>
 						</div>
